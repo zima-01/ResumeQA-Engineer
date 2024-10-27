dac4d800b356a0ce0054c2f2959608e6ceb9a937
Dmitri Zimarev <76609312+zima-01@users.noreply.github.com>
2024-10-27 11:05:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".scroll").addEventListener("click", function() {
+    // Scroll smoothly to the target section
+    document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+});
